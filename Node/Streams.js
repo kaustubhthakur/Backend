f467680a3add0bbdd4a4cc219c0e16a05700c7c1
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-11 13:01:58 +0530

    sixth commit

--- Node/Streams.js
+++ Node/Streams.js
@@ -1,10 +1,7 @@
 const http = require('http');
 const {writeFileSync, createReadStream} = require('fs');
 
-const server = http.createServer((req,res) => {
 
-
-}) 
 
 server.listen(5000,() => {
     console.log('server is running on port 5000')
