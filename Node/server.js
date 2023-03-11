b1aab2f34298227f13c0a46ad9772f3ef30d17c7
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-11 12:09:36 +0530

    sixth commit

--- Node/server.js
+++ Node/server.js
@@ -1,1 +1,15 @@
-console.log('hello ')
+const http = require('http');
+const port = 5000;
+const app = http.createServer((req, res) => {
+   if (req.url == '/') {
+      res.end('welcome to home page');
+   }
+   if (req.url == '/about') {
+      res.end('welcome to about page');
+   }
+ 
+})
+app.listen(port, (req, res) => 
+{
+   console.log(`server is running on port ${port}...`);
+})
